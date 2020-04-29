ce7633e4cf2d398f208f8963046676752cbd968a
Kat-NB <45222045+katawful@users.noreply.github.com>
2020-04-29 11:07:44 -0400

    Delete test.py

--- i3/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-var = subprocess.Popen(['/home/kat/.config/i3/battery.sh'], stdout=subprocess.PIPE)
-output = var.stdout.read()
-output = output[:-1]
-print(output)
